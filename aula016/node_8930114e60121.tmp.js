23c2b67305cdf2d607a46a3d16380abb3e48e730
Gabriel Madureira <gabriel.madureira2610@gmail.com>
2025-05-12 19:31:33 -0300

    Estrutura de repetição
    Apenas uma aula com ensinando como funciona uma estrutura de repetição (while, do while)

---
+++ aula016/node_8930114e60121.tmp.js
@@ -0,0 +1,13 @@
+/*
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+*/
+
+var c = 1
+while (c < 6){
+    console.log('tudo bem?')
+}
